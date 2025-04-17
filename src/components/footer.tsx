4770ffaa1bfe9f63964752caa239aabc40d51fd9
Victor Idam <idamvictorx1@gmail.com>
2025-04-17 16:22:28 +0100

    provided a miniature version for the plan button on the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,16 @@
-import {Crown, Mail, Settings, Volume2} from "lucide-react";
+import { Crown, Mail, Settings, Volume2 } from "lucide-react";
 import { useState, useEffect } from "react";
 import { Maximize, Minimize } from "react-feather";
 import { Link } from "react-router-dom";
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from "@/components/ui/button.tsx";
 import AccountPlansModal from "@/components/AccountPlanModal.tsx";
+import { useMobile } from "@/hooks/use-mobile.tsx";
 
 export default function Footer() {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
   const [isFullScreen, setIsFullScreen] = useState(false);
-
   const [isPlansModalOpen, setIsPlansModalOpen] = useState(false);
-
+  const isMobile = useMobile();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,19 +33,29 @@ export default function Footer() {
   return (
     <div className="flex justify-between items-center bg-background text-muted-foreground p-2 text-xs border-t-2 border-secondary mt-2">
       {/* Left Section */}
-      <div className="hidden md:flex">
+      <div className="flex">
         <Button
-            onClick={() => setIsPlansModalOpen(true)}
-            className="bg-gradient-to-r from-red-500 to-red-400 hover:from-red-500 hover:to-red-400 text-black font-medium rounded-md border border-red-300 shadow-md transition-all duration-300 group"
+          onClick={() => setIsPlansModalOpen(true)}
+          className={
+            isMobile
+              ? "w-8 h-8 bg-gradient-to-r from-red-500 to-red-400 hover:from-red-500 hover:to-red-400 p-1 rounded-md border border-red-300 shadow-md transition-all duration-300 group"
+              : "bg-gradient-to-r from-red-500 to-red-400 hover:from-red-500 hover:to-red-400 text-black font-medium rounded-md border border-red-300 shadow-md transition-all duration-300 group"
+          }
         >
-          <div className="flex items-center gap-3">
-            <div className="flex items-center justify-center w-5 h-5 rounded-full bg-white/20 backdrop-blur-sm">
-              <Crown className="h-2 w-2 text-white group-hover:scale-110 transition-transform" />
+          {isMobile ? (
+            <Crown className="h-4 w-4 text-white group-hover:scale-110 transition-transform" />
+          ) : (
+            <div className="flex items-center gap-3">
+              <div className="flex items-center justify-center w-5 h-5 rounded-full bg-white/20 backdrop-blur-sm">
+                <Crown className="h-2 w-2 text-white group-hover:scale-110 transition-transform" />
+              </div>
+              <div className="flex flex-col items-start">
+                <span className="text-xs text-white font-bold">
+                  BASIC ACCOUNT
+                </span>
+              </div>
             </div>
-            <div className="flex flex-col items-start">
-              <span className="text-xs text-white font-bold">BASIC ACCOUNT</span>
-            </div>
-          </div>
+          )}
         </Button>
       </div>
 
@@ -77,8 +87,8 @@ export default function Footer() {
         </div>
       </div>
       <AccountPlansModal
-          open={isPlansModalOpen}
-          onOpenChange={setIsPlansModalOpen}
+        open={isPlansModalOpen}
+        onOpenChange={setIsPlansModalOpen}
       />
     </div>
   );
