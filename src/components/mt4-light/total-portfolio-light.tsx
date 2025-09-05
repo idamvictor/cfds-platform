65480941970b1db6bf1201d660e4b913cfc4b954
Victor Idam <idamvictorx1@gmail.com>
2025-09-05 06:37:59 +0100

    completed the ui for the trading portfolio

--- src/components/mt4-light/total-portfolio-light.tsx
+++ src/components/mt4-light/total-portfolio-light.tsx
@@ -2,53 +2,60 @@
 import useTradeStore from "@/store/tradeStore";
 import { useCurrency } from "@/hooks/useCurrency";
 import { cn } from "@/lib/utils";
-import { Button } from "../ui/button";
-import { useNavigate } from "react-router-dom";
+import { Dot } from "lucide-react";
+// import { Button } from "../ui/button";
+// import { useNavigate } from "react-router-dom";
 
 export default function TotalPortfolioLight() {
   const accountSummary = useTradeStore((state) => state.accountSummary);
   const { formatCurrency } = useCurrency();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
-    <div className="bg-[#1C2030] border-t border-slate-700 p-4 w-full">
+    <div className="bg-[#C3C3C3] border-t border-slate-700 w-full p-1">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-8 flex-1 min-w-0">
-          <span className="text-sm font-medium text-white whitespace-nowrap">
-            {/* Total Portfolio */}
-          </span>
-          <div className="overflow-x-auto pb-2 flex-1 min-w-0">
-            <div className="flex gap-6 text-sm min-w-max">
-              <div>
-                <span className="text-gray-400">BALANCE</span>
-                <span className="ml-2 text-white">
+          {/* <span className="text-sm font-medium text-slate-900 whitespace-nowrap">
+            Total Portfolio
+          </span> */}
+
+          <div className="overflow-x-auto flex-1 min-w-0">
+            <div className="flex gap-6 items-center text-xs min-w-max">
+              <div className="flex items-center gap-2">
+                <Dot className="text-slate-900" />
+                <span className="text-slate-900 font-semibold">BALANCE:</span>
+                <span className="ml-2 text-slate-900 font-semibold">
                   {formatCurrency(accountSummary.balance)}
                 </span>
               </div>
               <div>
-                <span className="text-gray-400">EQUITY</span>
-                <span className="ml-2 text-white">
+                <span className="text-slate-900 font-semibold">EQUITY:</span>
+                <span className="ml-2 text-slate-900 font-semibold">
                   {formatCurrency(accountSummary.equity)}
                 </span>
               </div>
               <div>
-                <span className="text-gray-400">MARGIN</span>
-                <span className="ml-2 text-white">
+                <span className="text-slate-900 font-semibold">MARGIN:</span>
+                <span className="ml-2 text-slate-900 font-semibold">
                   {formatCurrency(accountSummary.margin)}
                 </span>
               </div>
               <div>
-                <span className="text-gray-400">FREE MARGIN</span>
-                <span className="ml-2 text-white">
+                <span className="text-slate-900 font-semibold">
+                  FREE MARGIN:
+                </span>
+                <span className="ml-2 text-slate-900 font-semibold">
                   {formatCurrency(accountSummary.freeMargin)}
                 </span>
               </div>
               <div>
-                <span className="text-gray-400">P/L</span>
+                <span className="text-slate-900 font-semibold">P/L:</span>
                 <span
                   className={cn(
-                    "ml-2",
-                    accountSummary.pnl >= 0 ? "text-green-500" : "text-red-500"
+                    "ml-2 font-semibold",
+                    accountSummary.pnl >= 0
+                      ? "text-slate-900"
+                      : "text-slate-900"
                   )}
                 >
                   {accountSummary.pnl >= 0 ? "" : "-"}
@@ -58,12 +65,12 @@ export default function TotalPortfolioLight() {
             </div>
           </div>
         </div>
-        <Button
+        {/* <Button
           className="bg-orange-600  hover:bg-orange-700 ml-4 shrink-0"
           onClick={() => navigate("/main/dashboard")}
         >
           DASHBOARD
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
